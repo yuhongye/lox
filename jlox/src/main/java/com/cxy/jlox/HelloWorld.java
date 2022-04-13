ad1619070292fa6ae8e7be6126f0707ed1fb7e37
喻红叶 <caoxyemail@163.com>
2022-04-13 20:21:28 +0800

    构架java脚手架

---
+++ jlox/src/main/java/com/cxy/jlox/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.cxy.jlox;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HelloWorld {
+    public static void main(String[] args) {
+        log.info("Hello, World!");
+    }
+}
